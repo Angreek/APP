545ade2c60e5f0b8de58d77405161b0076423dff
Angreek <andrew.kukhar@gmail.com>
2018-08-17 15:16:44 +0300

    4 task

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,13 @@
-function calc(a, b, method) {
-    return method(a, b);
-}
-
-function add(x, y) {
-    return x + y;
-}
-
-function substract(x, y) {
-    return x - y;
-}
-
-function mult(x, y) {
-    return x * y;
-}
-
-function division(x, y) {
-    return x / y;
-}
-
-function modulus(x, y) {
-    return x % y;
-
-}
-
-function pow(x, y) {
-    if (y >= 1) {
-        res = x;
-        for (i = 1, i <= y, i++) {
-            return res *= x;
-        } else {
-        console.log("the exponent has to be integer and bigger then 0")
+function onDeposit(pervVznos, procStavka, termInYears) {
+    let total = pervVznos;
+    for (i = 0; i < termInYears; i++) {
+        total = total * (100 + procStavka) / 100;
     }
-}
-console.log(calc(15,6, add1));
-console.log(calc(15,6, substract1));
-console.log(calc(15,6, mult));
-console.log(calc(15,6, division));
-console.log(calc(15,6, modulus));
-console.log(calc(15,6, pow));
+    return total;
+};
+
+let pervVznos = 100;//+prompt("Первонач взнос");
+let procStavka = 5;//+prompt("проц ставка в процентах");
+let termInYears = 10;//+prompt("Срок в годах");
+
+console.log(onDeposit(pervVznos, procStavka, termInYears));
